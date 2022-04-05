98aae7d819c667954f5fed2f53c0f0247a6ed776
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-05 15:19:07 +0000

    add comments

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ while True:
         break
     else:
         print("Password is not fine")
+
+'''
+We're using a while  loop here because we need to keep the program running until the user submits a password that satisfies all three conditions. Line 3 contains the three conditions connected with an and  operator. Line 3 becomes True  only when all three conditions are True . If that happens, Password is fine  is generated, and the break  statement will break the loop, and the program will stop. If at least one of the conditions in Line 3 is False Line 3 evaluates to False  and the print  statement under else is executed, and the loop starts over again.
+'''
